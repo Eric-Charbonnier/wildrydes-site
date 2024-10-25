20e7b0b428c4ffde4a87cd63cc05d3e006937e77
Eric Charbonnier <eric.charbonnier.dev@gmail.com>
2024-10-25 15:45:16 -0400

    update config file

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_6UkwBv27P', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7thb0ej6vac6eharsr9drujrkb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
